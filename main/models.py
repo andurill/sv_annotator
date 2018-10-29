6a3e331453eea3bba48628c504a85819b619bffc
andurill <gowtham.jn@gmail.com>
2018-10-29 11:35:09 -0400

    Added more exceptions

--- main/models.py
+++ main/models.py
@@ -18,20 +18,70 @@ class MultipleCytoBandError(Error):
             self, "Multiple cytobands identified for the breakpoint.")
 
 
-class CanonicalTranscriptError(Error):
+class CanonicalTranscriptNotFound(Error):
     '''Raised when canonical transcript not found'''
 
+    def __init__(self, gene):
+        Exception.__init__(
+            self, "Cannot find a canonical transcript for the gene: " + gene)
+
+
+class IncorrectGenesFormat(Error):
+    '''Raised when gene1 / gene2 format does not match expected'''
+
     def __init__(self):
         Exception.__init__(
-            self, "Cannot find a canonical transcript for the breakpoint.")
+            self, "Input genes are not in required format. Should be in 'Gene1 / Gene2' format.")
 
 
-class MultipleCanonicalTxError(Error):
-    '''Raised when multiple canonical transcript versions exist'''
+class IncorrectBkpFormat(Error):
+    '''Raised when a bkp format does not match expected'''
 
-    def __init__(self):
+    def __init__(self, bkp):
         Exception.__init__(
-            self, "Multiple versions of canonical transcript exists.")
+            self, "Format of breakpoint " + str(bkp) + "does not match the required format. Example '7:1234567'.)
 
 
+class IncorrectBkpFormat(Error):
+    '''Raised when a bkp format does not match expected'''
+
+    def __init__(self, bkp):
+        Exception.__init__(
+            self, "Format of breakpoint " + str(bkp) + "does not match the required format. Example '7:1234567'.)
 #define reamining exception
+
+
+class IncorrectDescriptionFormat(Error):
+    '''Raised when the input fusion description does not match expected'''
+
+    def __init__(self):
+        Exception.__init__(
+            self, "Format of fusion description does not meet the required format. Example 'Protein Fusion: {FGFR3:TACC3}'"
+        )
+
+
+class GenesNotInPanel(Error):
+    '''Raised when both genes not in panel'''
+
+    def __init__(self):
+        Exception.__init__(
+            self, "Neither of the gene partners is in the target panel."
+        )
+
+
+class FusionGeneConflict(Error):
+    '''Raised when one of the fusion parter does not match either of the input genes'''
+
+    def __init__(self, gene):
+        Exception.__init__(
+            self, "The fusion partner, " + str(gene) + " does not match either of the two input genes."
+        )
+
+
+class cdnaNotFound(Error):
+    '''Raised when vep cannot generate cdna annotation for a breakpoint'''
+
+    def __init__(self, bkp):
+        Exception.__init__(
+            self, "Cannot determine cDNA annotation for the breakpoint: " + bkp
+        )
