ba349dfd6290b100769e0292f06961f8db6e36fc
andurill <gowtham.jn@gmail.com>
2019-01-16 09:29:10 -0500

    udpate to SV model

--- main/models.py
+++ main/models.py
@@ -1,108 +1,100 @@
+class bkp(object):
+    def __init__(self, chrom, pos, gene, desc):
+        try:
+            self.chr = str(chrom)
+            self.pos = int(pos)
+            self.gene = str(gene)
+            self.desc = str(desc)
+        except TypeError:
+            print("Could not create a new instance of bkp class due to inappropriate values for parameters.")
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
+
+    def __expand(self, target_panel, panelKinase, refFlat):
+        if refFlat[self.gene]:
+            self.transcript = refFlat[self.id]
+        else:
+            raise CanonicalTranscriptNotFound(self.id)
+
+        if target_panel[self.gene]:
+            self.isPanel = True
+        else:
+            self.isPanel = False
+
+        if panelKinase[self.gene]:
+            self.isKinase = True
+        else:
+            self.isKinase = False
+
+
 class sv(object):
     def __init__(self, svtype, bkp1, bkp2, genes, site1, site2, description, connection):
-       self.__svtype = svtype
-       self.__bkp1 = bkp1
-       self.__bkp2 = bkp2
-       self.__genes = genes
-       self.__site1 = site1
-       self.__site2 = site2
-       self.__description = description
-       self.__connection = connection
-
-    # Define key variables
-
-    def __set_variables(self, target_panel, panelKinase, oncoKb, refFlat):
+        self.__svtype = svtype
+        self.__site1 = site1
+        self.__site2 = site2
+        self.__description = description
+        self.__connection = connection
         try:
-            self.__gene1, self.__gene2 = genes.split(" / ")
+            self.__chr1, self.__pos1 = self.__bkp1.split(":") #IncorrectBkpFormat()
+            self.__chr2, self.__chr2 = self.__bkp2.split(":") #IncorrectBkpFormat()
+            self.__gene1, self.__gene2 = genes.split(" / ") #IncorrectGenesFormat()
         except ValueError:
-            raise IncorrectGenesFormat()
+            print("Could not create a new instance of sv class due to inappropriate values for parameters.")
 
-        try:
-            self.__chr1, self.__pos1 = self.__bkp1.split(":")
-        except ValueError:
-            raise IncorrectBkpFormat()
+    def __expand(self):
+        self.bkp1 = bkp(self.__chr1, self.__pos1, self.__gene1, self.__site1)
+        self.bkp2 = bkp(self.__chr2, self.__pos2, self.__gene2, self.__site2)
 
-        try:
-            self.__chr2, self.__pos2 = self.__bkp2.split(":")
-        except ValueError:
-            raise IncorrectBkpFormat()
-
-        # get canonical transcript(s) for gene1, otherwise raise exception
-        if refFlat[self.__gene1]:
-            self.__transcript1 = refFlat[self.__gene1]
-        else:
-            raise CanonicalTranscriptNotFound(self.__gene1)
-
-        # get canonical transcript(s) for gene2, otherwise raise exception
-        if refFlat[self.__gene2]:
-            self.__transcript2 = refFlat[self.__gene2]
-        else:
-            raise CanonicalTranscriptNotFound(self.__gene2)
-
-        # Is gene1 in panel?
-        if target_panel[self.__gene1]:
-            self.__isGene1InPanel = True
-        else:
-            self.__isGene1InPanel = False
-
-        # Is gene2 in panel?
-        if target_panel[self.__gene2]:
-            self.__isGene2InPanel = True
-        else:
-            self.__isGene2InPanel = False
-
-        if self.__isGene1InPanel is False and self.__isGene2InPanel is False:
+        # Define key variables
+        if self.bkp1.InPanel is False and self.bkp2.InPanel is False:
             raise GenesNotInPanel()
 
         # Is SV intragenic?
-        if self.__gene1 == self.__gene2:
+        if self.bkp1.gene == self.bkp2.gene:
             self.__isIntragenic = True
         else:
             self.__isIntragenic = False
 
-        # Are one or both genes in panel and kinase?
-        self.__kinase_genes = set()
-        for gene in self.__gene1, self.__gene2:
-            if target_panel[gene] and panelKinase[gene]:
-                self.__kinase_genes.add(gene)
-
-        # IF PROTEIN FUSION
+        # Fusion variables
         if self.__description.startswith("Protein Fusion: "):
             self.__isFusion = True
             s = self.__description
             self.__fusionGene = s[s.find("{")+1:s.find("}")]
             # check if fusion is defined in the correct format Gene1:Gene2
             try:
-                self.__fusionPartner1, self.__fusionPartner2 = self.__fusionGene.split(
-                    ":")
+                self.__fusionPartner1, self.__fusionPartner2 = self.__fusionGene.split(":")
             except ValueError:
                 raise IncorrectDescriptionFormat()
 
             # Check if fusion partners match the genes provided as inputs
-            for gene in (self.__fusionPartner1, self.__fusionPartner2):
-                if gene not in (self.__gene1, self.__gene2):
-                    raise FusionGeneConflict(gene)
-            if self.__fusionPartner1 == self.__gene1:
-                self.__fusionTranscript1 = self.__transcript1
-                self.__fusionTranscript2 = self.__transcript2
+            if self.__fusionPartner1 == self.bkp1.gene and self.__fusionPartner2 == self.bkp2.gene:
+                self.__fusionPartner1, self.__fusionPartner2 = self.bkp1, self.bkp2
+            elif self.__fusionPartner2 == self.bkp1.gene and self.__fusionPartner1 == self.bkp2.gene:
+                self.__fusionPartner2, self.__fusionPartner1 = self.bkp1, self.bkp2      
             else:
-                self.__fusionTranscript2 = self.__transcript1
-                self.__fusionTranscript1 = self.__transcript2
+                raise FusionGeneConflict()         
 
             # Is fusion known?
             if self.__fusionGene in oncoKb:
                 self.__isKnownFusion = True
             else:
                 self.__isKnownFusion = False
-
-        # IF NOT FUSION
         else:
             self.__fusionGene = None
             self.__isFusion = False
             self.__isKnownFusion = False
 
-    # define breakpoint order   (NOT USED AT THIS TIME)
+        # Annotation variables
+        if self.__svtype == "TRANSLOCATION":
+            if self.bkp1.chrom.isdigit() and self.bkp2.chrom.isdigit():
+                if int(self.bkp1.chrom) < int(self.bkp1.chrom):
+                    self.__annotationPartner1, self.__annotationPartner2 = self.bkp1, self.bkp2
+                else:
+                    self.__annotationPartner1, self.__annotationPartner2 = self.bkp2, self.bkp1
+            elif 
 
+            
     def __set_bkpOrder(self):
         self.__bkpOrder = {}
         self.__tranlocationCoordOrder = {}
@@ -150,34 +142,9 @@ class sv(object):
             else:
                 raise BreakpointConflictForTranslocation()
 
-# not used at the moment
 
 
-class gene(object):
-    def __init__(self, geneID, bkp):
-        self.id = geneID
-        self.bkp = bkp
 
-    def __expand(self, target_panel, panelKinase, refFlat):
-        try:
-            self.chr, self.pos = self.bkp.split(":")
-        except ValueError:
-            raise IncorrectBkpFormat()
-
-        if refFlat[self.id]:
-            self.transcript = refFlat[self.id]
-        else:
-            raise CanonicalTranscriptNotFound(self.id)
-
-        if target_panel[self.id]:
-            self.isPanel = True
-        else:
-            self.isPanel = False
-
-        if panelKinase[self.id]:
-            self.isKinase = True
-        else:
-            self.isKinase = False
 
 
 
