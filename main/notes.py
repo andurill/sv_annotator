f920ec3291a5853594e941da6e052b98e359b5bd
andurill <gowtham.jn@gmail.com>
2019-01-29 08:49:23 -0500

    udpates to notes

--- main/notes.py
+++ main/notes.py
@@ -1,19 +1,26 @@
-import os, sys
+#!usr/bin/env python2
+
+import os
+import sys
 import pandas as pd
 import numpy as np
 from notes_constants import *
 
 
 refFlat_summary = pd.read_csv("./data/refFlat_summary.txt", sep="\t", dtype={
-                              'a': str, 'b': str, 'c': str, 'd': str, 
+                              'a': str, 'b': str, 'c': str, 'd': str,
                               'e': int, 'f': int, 'g': int})
 
 
 def get_bkp_info(bkp):
-    #bkp.firstexon = refFlat_summary[refFlat_summary.Gene ==
+    """
+    Get exon and intron features for a breakpoint object.
+    bkp -> None
+    """
+    # bkp.firstexon = refFlat_summary[refFlat_summary.Gene ==
     #                               "EGFR"].first_exon.tolist().pop(0)
     bkp_dict = refFlat_summary[
-        (refFlat_summary['Gene'].values == bkp.gene) & 
+        (refFlat_summary['Gene'].values == bkp.gene) &
         (refFlat_summary['Transcript'].values == bkp.Transcript)]\
         .to_dict('records').pop()
     bkp.firstexon = bkp_dict['first_exon']
@@ -37,6 +44,11 @@ def get_bkp_info(bkp):
 
 
 def get_exons_involved(sv):
+    """
+    Get exons involved in an sv object based on the variant type
+    and the breakpoint sites
+    sv -> None
+    """
     if sv.isFusion:
         get_bkp_info(sv.fusionPartner1)
         get_bkp_info(sv.fusionPartner2)
@@ -46,6 +58,10 @@ def get_exons_involved(sv):
         note2 = "%s exons %s - %s" % (sv.fusionPartner2.gene,
                                       sv.fusionPartner2.exon,
                                       sv.fusionPartner2.lastexon)
+        sv.fusionPartner1.variantSite1 = sv.fusionPartner1.firstexon
+        sv.fusionPartner1.variantSite2 = sv.fusionPartner1.exon
+        sv.fusionPartner2.variantSite1 = sv.fusionPartner2.firstexon
+        sv.fusionPartner2.variantSite2 = sv.fusionPartner2.exon
     elif sv.bkp1.isPanel and sv.bkp2.isPanel and sv.bkp1.isCoding and sv.bkp2.isCoding:
         get_bkp_info(sv.annotationPartner1)
         get_bkp_info(sv.annotationPartner2)
@@ -54,41 +70,43 @@ def get_exons_involved(sv):
                                          sv.annotationPartner1.site,
                                          sv.annotationPartner2.gene,
                                          sv.annotationPartner2.site)
+        elif sv.isIntragenic:
+            note1 = "%s exons %s - %s" % (sv.annotationPartner1.gene,
+                                          sv.annotationPartner1.exon,
+                                          sv.annotationPartner2.exon)
         else:
             note1 = "%s exons %s - %s" % (sv.annotationPartner1.gene,
                                           sv.annotationPartner1.firstexon,
                                           sv.annotationPartner1.exon)
-        note2 = "%s exons %s - %s" % (sv.fusionPartner2.gene,
-                                      sv.fusionPartner2.exon,
-                                      sv.fusionPartner2.lastexon)
-        gene1, tx1, cdna1 = sv.annotationPartner1.gene, \
-            sv.annotationPartner1.transcript, sv.annotationPartner1.cdna
-        gene2, tx2, cdna2 = sv.annotationPartner2.gene, \
-            sv.annotationPartner2.transcript, sv.annotationPartner2.cdna
-        if sv.isIntragenic:
-            return "%s (%s) %s: %s_%s:%s" %\
-                (gene1, tx1, fusion_type, cdna1, cdna2, svtype)
-        else:
-            return "%s (%s) - %s (%s) %s: %s:%s_%s:%s%s" %\
-                (gene1, tx1, gene2, tx2, fusion_type,
-                 cdna1, gene1, cdna2, gene2, svtype)
+            note2 = "%s exons %s - %s" % (sv.annotationPartner2.gene,
+                                          sv.annotationPartner2.exon,
+                                          sv.annotationPartner2.lastexon)
     elif sv.bkp1.isPanel and sv.bkp1.isCoding:
-        gene1, tx1, cdna1 = sv.bkp1.gene, sv.bkp1.transcript, sv.bkp1.cdna
-        if not cdna1.startswith("chr"):
-            cdna2 = "chr" + sv.bkp2.chrom + ":g." + str(sv.bkp2.pos)
-            return "%s (%s) %s: %s:%s_%s%s" %\
-                (gene1, tx1, fusion_type, cdna1, gene1, cdna2, svtype)
-        else:
-            raise BreakPointIntergenic(sv.bkp1)
+        if sv.svtype == "TRANSLOCATION":
+            note1 = "%s %s" % (sv.annotationPartner1.gene,
+                               sv.annotationPartner1.site)
+        elif sv.bkp1.strand == "+":
+            note1 = "%s exons %s - %s" % (sv.annotationPartner1.gene,
+                                          sv.annotationPartner1.exon,
+                                          sv.annotationPartner1.lastexon)
+        elif sv.bkp1.strand == "-":
+            note1 = "%s exons %s - %s" % (sv.annotationPartner1.gene,
+                                          sv.annotationPartner1.firstexon,
+                                          sv.annotationPartner1.exon)
     else:
-        gene2, tx2, cdna2 = sv.bkp2.gene, sv.bkp2.transcript, sv.bkp2.cdna
-        if not cdna2.startswith("chr"):
-            cdna1 = "chr" + sv.bkp1.chrom + ":g." + str(sv.bkp1.pos)
-            return "%s (%s) %s: %s:%s_%s%s" %\
-                (gene2, tx2, fusion_type, cdna2, gene2, cdna1, svtype)
-        else:
-            raise BreakPointIntergenic(sv.bkp2)
+        if sv.svtype == "TRANSLOCATION":
+            note1 = "%s %s" % (sv.annotationPartner2.gene,
+                               sv.annotationPartner2.site)
+        elif sv.bkp2.strand == "+":
+            note1 = "%s exons %s - %s" % (sv.annotationPartner2.gene,
+                                          sv.annotationPartner2.exon,
+                                          sv.annotationPartner2.lastexon)
+        elif sv.bkp2.strand == "-":
+            note1 = "%s exons %s - %s" % (sv.annotationPartner2.gene,
+                                          sv.annotationPartner2.firstexon,
+                                          sv.annotationPartner2.exon)
     return
+
 
 def get_note(sv):
     note = None
@@ -96,28 +114,54 @@ def get_note(sv):
 
 
 def get_prefix(sv):
+    """
+    Get the prefix of a note based on variant type
+    sv -> str
+    """
     prefix = "Note: The "
     fusion_type = "rearrangement"
-    if sv.isFusion:
-        if sv.isKnownFusion:
-            fusion_type = "fusion"
-        prefix += "%s - %s " % (sv.fusionPartner)
-    return
+    if sv.isKnownFusion:
+        fusion_type = "fusion"
+        prefix += str(sv.annotation.split(":")[0]) + \
+            fusion_type + " involves "
+    elif sv.isFusion:
+        prefix += str(sv.annotation.split(":")[0]) + \
+            fusion_type + " is a " + sv.svtype.lower() + \
+            "that results in a fusion of "
+    elif sv.isIntragenic:
+        prefix += str(sv.annotation.split(":")[0]) + \
+            fusion_type + " is an intragenic " + \
+            sv.svtype.lower() + " of "
+    else:
+        prefix += str(sv.annotation.split(":")[0]) + \
+            fusion_type + " is a " + sv.svtype.lower() + " of "
+    return prefix
 
 
-def get_kinase(sv):
+def get_fusion_misc(sv):
+    """
+    get frame and kinase domain information for
+    fusion variants only
+    sv -> str
+    """
     for bkp in (sv.bkp1, sv.bkp2):
         if bkp.isKinase:
+            return
 
-    
 
 def functional_significance(sv):
+    """
+    Determine if functional significace note is neccessary
+    based on variant type
+    sv -> str
+    """
     if sv.isKnownFusion or\
-    (sv.isFusion and ((sv.bkp1.isHotspot and sv.bkp1.isEntireKinase) or\
-    (sv.bkp2.isHotspot and sv.bkp2.isEntireKinase))) or\
-    (sv.svtype == "DELETION" and\
-    ((sv.bkp1.isPanel and sv.bkp1.isTumourSuppressor) or\
-    (sv.bkp2.isPanel and sv.bkp2.isTumourSuppressor))):
+        (sv.isFusion and
+         ((sv.fusionPartner1.isHotspot and sv.fusionPartner1.isEntireKinase) or
+          (sv.fusionPartner2.isHotspot and sv.fusionPartner2.isEntireKinase))) or\
+        (sv.svtype == "DELETION" and
+         ((sv.bkp1.isPanel and sv.bkp1.isTumourSuppressor) or
+          (sv.bkp2.isPanel and sv.bkp2.isTumourSuppressor))):
         return ""
     else:
         return " The functional significance is undetermined."
