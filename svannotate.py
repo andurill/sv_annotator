deb17ff4cbbdb8684477899b488c5a52177218c6
andurill <gowtham.jn@gmail.com>
2019-12-09 00:54:03 -0500

    :zap: Enable multiprocessing. TODO: add logging

--- svannotate.py
+++ svannotate.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-'''
+"""
 @Description: Generate human-readable interpretations of structural variants.
 @author: jayakumg@mkscc.org
 @version: 1.0.0
-'''
+"""
 
 
 import os
@@ -15,6 +15,9 @@ import pandas as pd
 import numpy as np
 import logging
 import StringIO
+import argparse
+import multiprocessing as mp
+import contextlib
 from main.constants import *
 from main.models import sv
 from main.annotation import get_variant_annotation
@@ -28,47 +31,37 @@ tumourSuppressor = IMPACT_TumourSuppressors
 hotspot = IMPACT_Hotspots
 
 
-def annotate_SV(raw):
-    note, annotation, position = [None]*3
-    try:
-        svtype, bkp1, bkp2, genes, site1, \
-            site2, description = raw.split(",")
-    except ValueError as e:
-        logger.critical(e)
-        return note, annotation, position
+def main():
+    parser = argparse.ArgumentParser(description="SV annotator")
+    analysis_type = parser.add_mutually_exclusive_group(required=True)
+    # single sv processing
+    analysis_type.add_argument(
+        "-sv",
+        "--structural_variant",
+        type=str,
+        action="store",
+        help="Structural variant string",
+    )
+    # batch processing
+    analysis_type.add_argument(
+        "-i",
+        "--input_file",
+        type=argparse.FileType("r"),
+        action="store",
+        help="input file of multiple structural variants",
+    )
+    parser.add_argument(
+        "-o",
+        "--out_file",
+        type=argparse.FileType("w"),
+        action="store",
+        default=os.path.join(os.getcwd(), "SVannotated_results.txt"),
+        help="output file to print annotation results for multiple structural variants",
+    )
+    args = parser.parse_args()
 
-    try:
-        variant = sv(svtype, bkp1, bkp2, genes, site1, site2, description)
-    except Exception as e:
-        logger.critical(e)
-        return note, annotation, position
-
-    try:
-        variant.expand(transcript_reference, kinase_annotation,
-                         hotspot, tumourSuppressor, oncoKb)
-    except Exception as e:
-        logger.critical(e)
-        return note, annotation, position
-
-    try:
-        annotation = get_variant_annotation(variant)
-        sv.annotation = annotation
-    except Exception as e:
-        logger.critical(e)
-        return note, annotation, position
-
-    try:
-        annotation, note, position = get_notes(variant, refFlat_summary, kinase_annotation)
-    except Exception as e:
-        logger.critical(e)
-        return note, annotation, position
-
-    return note, annotation, position
-
-
-if __name__ == "__main__":
     # Create the logger
-    logger = logging.getLogger('basic_logger')
+    logger = logging.getLogger("basic_logger")
     logger.setLevel(logging.DEBUG)
 
     # Setup the console handler with a StringIO object
@@ -80,21 +73,139 @@ if __name__ == "__main__":
     logger.addHandler(ch)
 
     # Call main function
-    note, annotation, position = annotate_SV(sys.argv[1])
+    if args.input_file:
+        svdata = pd.read_csv(
+            args.input_file,
+            header="infer",
+            sep="\t",
+            dtype=str,
+            usecols=[
+                "TumorId",
+                "NormalId",
+                "Chr1",
+                "Pos1",
+                "Chr2",
+                "Pos2",
+                "SV_Type",
+                "Gene1",
+                "Gene2",
+                "Site1Description",
+                "Site2Description",
+                "Fusion",
+            ],
+        )
+        svdata["coord1"] = svdata["Chr1"] + ":" + svdata["Pos1"]
+        svdata["coord2"] = svdata["Chr2"] + ":" + svdata["Pos2"]
+        svdata["Genes"] = svdata["Gene1"] + " / " + svdata["Gene2"]
+        svdata["SV_Type"] = svdata["SV_Type"].apply(
+            lambda x: "INVERSION"
+            if x == "INV"
+            else (
+                "TRANSLOCATION"
+                if x == "TRA"
+                else ("DELETION" if x == "DEL" else "DUPLICATION")
+            )
+        )
+        svdata = svdata.drop(["Chr1", "Chr2", "Pos1", "Pos2"], axis=1)
+        col = [
+            u"SV_Type",
+            u"coord1",
+            u"coord2",
+            u"Genes",
+            u"Site1Description",
+            u"Site2Description",
+            u"Fusion",
+        ]
+        sv_strings = (
+            svdata[col]
+            .apply(lambda row: ",".join(row.values.astype(str)), axis=1)
+            .tolist()
+        )
+
+        cores = int(mp.cpu_count() - 1)
+        try:
+            # create jobs
+            with contextlib.closing(mp.Pool(processes=cores)) as pool:
+                annotated_SVs = pool.map(annotate_SV, sv_strings)
+        except Exception as e:
+            print(e)
+
+        new = pd.concat(
+            [
+                svdata,
+                pd.DataFrame(annotated_SVs, columns=["Note", "Annotation", "Position"]),
+            ],
+            axis=1,
+        )
+
+        new.to_csv(args.out_file, header=True, sep="\t", index=False)
+    # note, annotation, position = annotate_SV(args.sv, logger)
 
     # Send log contents to a string and close the stream
-    log_contents = log_capture_string.getvalue()
-    log_capture_string.close()
+    # log_contents = log_capture_string.getvalue()
+    # log_capture_string.close()
 
-    if note and position and annotation:
-        status = "SUCCESS"
-    else:
-        status = "FAILED"
-    result = {
-        'Note': note,
-        'Annotation': annotation,
-        'Position': position,
-        'status': status,
-        'message': log_contents.replace("\n","; ")
-    }
-    print result
+    # if note and position and annotation:
+    #     status = "SUCCESS"
+    # else:
+    #     status = "FAILED"
+    # result = {
+    #     "Note": note,
+    #     "Annotation": annotation,
+    #     "Position": position,
+    #     "status": status,
+    #     "message": log_contents.replace("\n", "; "),
+    # }
+    # print(result)
+
+
+def annotate_SV(raw):
+    """[summary]
+    
+    Arguments:
+        raw {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
+    note, annotation, position = [None] * 3
+    try:
+        svtype, bkp1, bkp2, genes, site1, site2, description = raw.split(",")
+    except ValueError as e:
+        # logger.critical(e)
+        return note, annotation, position
+
+    try:
+        variant = sv(svtype, bkp1, bkp2, genes, site1, site2, description)
+    except Exception as e:
+        # logger.critical(e)
+        return note, annotation, position
+
+    try:
+        variant.expand(
+            transcript_reference, kinase_annotation, hotspot, tumourSuppressor, oncoKb
+        )
+    except Exception as e:
+        # logger.critical(e)
+        return note, annotation, position
+
+    try:
+        annotation = get_variant_annotation(variant)
+        sv.annotation = annotation
+    except Exception as e:
+        # logger.critical(e)
+        return note, annotation, position
+
+    try:
+        annotation, note, position = get_notes(
+            variant, refFlat_summary, kinase_annotation
+        )
+    except Exception as e:
+        # logger.critical(e)
+        return note, annotation, position
+
+    return (note, annotation, position)
+
+
+if __name__ == "__main__":
+    main()
