6da2713abb94028099fcc7e71a64be29b2dff04a
andurill <gowtham.jn@gmail.com>
2019-12-11 16:48:49 -0500

    include FASTA file path

--- main/config.py
+++ main/config.py
@@ -4,3 +4,4 @@ import sys
 VEP = "/dmp/hot/jayakumg/projects/germline-heme/vep/variant_effect_predictor.pl"
 VEP_CACHE = "/dmp/resources/prod/tools/bio/vep/VERSIONS/variant_effect_predictor_v86"
 PERL = "/dmp/resources/prod/tools/system/perl/bin/perl"
+FASTA = "/dmp/resources/prod/tools/bio/vep/VERSIONS/variant_effect_predictor_v86/homo_sapiens_merged/86_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa"
