5f80d26d0001d149570c629139bb6e2f98e0b36a
andurill <gowtham.jn@gmail.com>
2019-12-11 16:53:56 -0500

    linting

--- main/annotation.py
+++ main/annotation.py
@@ -5,7 +5,7 @@ import logging
 from constants import cb_df
 from models import BreakPointIntergenic
 
-logger = logging.getLogger('basic_logger')
+logger = logging.getLogger("basic_logger")
 
 
 def get_variant_annotation(sv):
@@ -32,27 +32,53 @@ def get_translocation(sv):
     if sv.isFusion:
         if sv.isKnownFusion is True:
             fusion_type = "fusion"
-        Annotation = "%s (%s) - %s (%s) %s: " %\
-            (sv.fusionPartner1.gene, sv.fusionPartner1.transcript,
-             sv.fusionPartner2.gene, sv.fusionPartner2.transcript, fusion_type)
-    elif all([sv.annotationPartner1.isPanel, sv.annotationPartner2.isPanel,
-              sv.annotationPartner1.isCoding, sv.annotationPartner2.isCoding]):
-        Annotation = "%s (%s) - %s (%s) %s: " %\
-            (sv.annotationPartner1.gene, sv.annotationPartner1.transcript,
-             sv.annotationPartner2.gene, sv.annotationPartner2.transcript, fusion_type)
+        Annotation = "%s (%s) - %s (%s) %s: " % (
+            sv.fusionPartner1.gene,
+            sv.fusionPartner1.transcript,
+            sv.fusionPartner2.gene,
+            sv.fusionPartner2.transcript,
+            fusion_type,
+        )
+    elif all(
+        [
+            sv.annotationPartner1.isPanel,
+            sv.annotationPartner2.isPanel,
+            sv.annotationPartner1.isCoding,
+            sv.annotationPartner2.isCoding,
+        ]
+    ):
+        Annotation = "%s (%s) - %s (%s) %s: " % (
+            sv.annotationPartner1.gene,
+            sv.annotationPartner1.transcript,
+            sv.annotationPartner2.gene,
+            sv.annotationPartner2.transcript,
+            fusion_type,
+        )
     elif sv.annotationPartner1.isPanel and sv.annotationPartner1.isCoding:
-        Annotation = "%s (%s) %s: " %\
-            (sv.annotationPartner1.gene, sv.annotationPartner1.transcript, fusion_type)
+        Annotation = "%s (%s) %s: " % (
+            sv.annotationPartner1.gene,
+            sv.annotationPartner1.transcript,
+            fusion_type,
+        )
     else:
-        Annotation = "%s (%s) %s: " %\
-            (sv.annotationPartner2.gene,
-             sv.annotationPartner2.transcript, fusion_type)
+        Annotation = "%s (%s) %s: " % (
+            sv.annotationPartner2.gene,
+            sv.annotationPartner2.transcript,
+            fusion_type,
+        )
 
     cband1 = get_cytoband(sv.annotationPartner1)
     cband2 = get_cytoband(sv.annotationPartner2)
-    t_format = sv.annotationPartner1.chrom, sv.annotationPartner2.chrom,\
-        cband1, cband2, sv.annotationPartner1.chrom, sv.annotationPartner1.pos,\
-        sv.annotationPartner2.chrom, sv.annotationPartner2.pos
+    t_format = (
+        sv.annotationPartner1.chrom,
+        sv.annotationPartner2.chrom,
+        cband1,
+        cband2,
+        sv.annotationPartner1.chrom,
+        sv.annotationPartner1.pos,
+        sv.annotationPartner2.chrom,
+        sv.annotationPartner2.pos,
+    )
     coordinate = "t(%s;%s)(%s;%s)(chr%s:g.%s::chr%s:g.%s)" % t_format
     return Annotation + coordinate
 
@@ -69,45 +95,109 @@ def get_other_svs(sv):
     if sv.isFusion and sv.bkp1.isCoding and sv.bkp2.isCoding:
         if sv.isKnownFusion:
             fusion_type = "fusion"
-        gene1, tx1, cdna1 = sv.fusionPartner1.gene, \
-            sv.fusionPartner1.transcript, sv.fusionPartner1.cdna
-        gene2, tx2, cdna2 = sv.fusionPartner2.gene, \
-            sv.fusionPartner2.transcript, sv.fusionPartner2.cdna
-        return "%s (%s) - %s (%s) %s: %s:%s_%s:%s%s" %\
-            (gene1, tx1, gene2, tx2, fusion_type,
-             cdna1, gene1, cdna2, gene2, svtype)
-    elif all([sv.annotationPartner1.isPanel, sv.annotationPartner2.isPanel,
-              sv.annotationPartner1.isCoding, sv.annotationPartner2.isCoding]):
-        gene1, tx1, cdna1 = sv.annotationPartner1.gene, \
-            sv.annotationPartner1.transcript, sv.annotationPartner1.cdna
-        gene2, tx2, cdna2 = sv.annotationPartner2.gene, \
-            sv.annotationPartner2.transcript, sv.annotationPartner2.cdna
+        gene1, tx1, cdna1 = (
+            sv.fusionPartner1.gene,
+            sv.fusionPartner1.transcript,
+            sv.fusionPartner1.cdna,
+        )
+        gene2, tx2, cdna2 = (
+            sv.fusionPartner2.gene,
+            sv.fusionPartner2.transcript,
+            sv.fusionPartner2.cdna,
+        )
+        return "%s (%s) - %s (%s) %s: %s:%s_%s:%s%s" % (
+            gene1,
+            tx1,
+            gene2,
+            tx2,
+            fusion_type,
+            cdna1,
+            gene1,
+            cdna2,
+            gene2,
+            svtype,
+        )
+    elif all(
+        [
+            sv.annotationPartner1.isPanel,
+            sv.annotationPartner2.isPanel,
+            sv.annotationPartner1.isCoding,
+            sv.annotationPartner2.isCoding,
+        ]
+    ):
+        gene1, tx1, cdna1 = (
+            sv.annotationPartner1.gene,
+            sv.annotationPartner1.transcript,
+            sv.annotationPartner1.cdna,
+        )
+        gene2, tx2, cdna2 = (
+            sv.annotationPartner2.gene,
+            sv.annotationPartner2.transcript,
+            sv.annotationPartner2.cdna,
+        )
         if sv.isIntragenic:
-            Annotation = "%s (%s) %s: %s_%s%s" %\
-                (gene1, tx1, fusion_type, cdna1, cdna2, svtype)
+            Annotation = "%s (%s) %s: %s_%s%s" % (
+                gene1,
+                tx1,
+                fusion_type,
+                cdna1,
+                cdna2,
+                svtype,
+            )
             return Annotation
         else:
-            Annotation = "%s (%s) - %s (%s) %s: %s:%s_%s:%s%s" %\
-                (gene1, tx1, gene2, tx2, fusion_type,
-                 cdna1, gene1, cdna2, gene2, svtype)
+            Annotation = "%s (%s) - %s (%s) %s: %s:%s_%s:%s%s" % (
+                gene1,
+                tx1,
+                gene2,
+                tx2,
+                fusion_type,
+                cdna1,
+                gene1,
+                cdna2,
+                gene2,
+                svtype,
+            )
             return Annotation
     elif sv.annotationPartner1.isPanel and sv.annotationPartner1.isCoding:
-        gene1, tx1, cdna1 = sv.annotationPartner1.gene, \
-            sv.annotationPartner1.transcript, sv.annotationPartner1.cdna
+        gene1, tx1, cdna1 = (
+            sv.annotationPartner1.gene,
+            sv.annotationPartner1.transcript,
+            sv.annotationPartner1.cdna,
+        )
         cdna2 = sv.annotationPartner2.cdna
-        Annotation = "%s (%s) %s: %s:%s_%s%s" %\
-            (gene1, tx1, fusion_type, cdna1, gene1,
-             "chr" + sv.annotationPartner2.chrom + ":g." +
-             str(sv.annotationPartner2.pos), svtype)
+        Annotation = "%s (%s) %s: %s:%s_%s%s" % (
+            gene1,
+            tx1,
+            fusion_type,
+            cdna1,
+            gene1,
+            "chr"
+            + sv.annotationPartner2.chrom
+            + ":g."
+            + str(sv.annotationPartner2.pos),
+            svtype,
+        )
         return Annotation
     else:
-        gene2, tx2, cdna2 = sv.annotationPartner2.gene, \
-            sv.annotationPartner2.transcript, sv.annotationPartner2.cdna
+        gene2, tx2, cdna2 = (
+            sv.annotationPartner2.gene,
+            sv.annotationPartner2.transcript,
+            sv.annotationPartner2.cdna,
+        )
         cdna1 = sv.annotationPartner1.cdna
-        Annotation = "%s (%s) %s: %s:%s_%s%s" %\
-            (gene2, tx2, fusion_type, cdna2, gene2,
-             "chr" + sv.annotationPartner1.chrom + ":g." +
-             str(sv.annotationPartner1.pos), svtype)
+        Annotation = "%s (%s) %s: %s:%s_%s%s" % (
+            gene2,
+            tx2,
+            fusion_type,
+            cdna2,
+            gene2,
+            "chr"
+            + sv.annotationPartner1.chrom
+            + ":g."
+            + str(sv.annotationPartner1.pos),
+            svtype,
+        )
         return Annotation
 
 
@@ -117,9 +207,11 @@ def get_cytoband(bkp):
     bkp -> str
     """
     chrom, coord = bkp.chrom, bkp.pos
-    which_cytoband = cb_df[(cb_df['Chr'].values == chrom) &
-                           (cb_df['Bp_start'].values <= coord) &
-                           (cb_df['Bp_stop'].values >= coord)]
+    which_cytoband = cb_df[
+        (cb_df["Chr"].values == chrom)
+        & (cb_df["Bp_start"].values <= coord)
+        & (cb_df["Bp_stop"].values >= coord)
+    ]
     if len(which_cytoband) == 0:
         raise MissingCytoBand(bkp)
     elif len(which_cytoband) > 1:
@@ -134,31 +226,28 @@ def reformat(svtype):
     str -> str
     """
 
-    svdict = {
-        "DUPLICATION": "dup",
-        "DELETION": "del",
-        "INVERSION": "inv"
-    }
+    svdict = {"DUPLICATION": "dup", "DELETION": "del", "INVERSION": "inv"}
     return svdict[svtype]
 
 
 class Error(Exception):
-    '''Base class for other exceptions'''
+    """Base class for other exceptions"""
+
     pass
 
 
 class MissingCytoBand(Error):
-    '''Raised when no cytoband was identified for a breakpoint'''
+    """Raised when no cytoband was identified for a breakpoint"""
 
     def __init__(self, bkp):
-        Exception.__init__(self, "No cytobands identified for the breakpoint: " + bkp
-                           )
+        Exception.__init__(self, "No cytobands identified for the breakpoint: " + bkp)
 
 
 class MultipleCytoBand(Error):
-    '''Raised when multiple cytoband were identified for a breakpoint'''
+    """Raised when multiple cytoband were identified for a breakpoint"""
 
     def __init__(self, bkp):
-        Exception.__init__(self,
-                           self, "Multiple cytobands identified for the breakpoint: " + bkp
-                           )
+        Exception.__init__(
+            self, self, "Multiple cytobands identified for the breakpoint: " + bkp
+        )
+
