e111f8ef352bd84aff5ad495c3e657f21854b77d
andurill <gowtham.jn@gmail.com>
2019-12-10 19:20:33 -0500

    :zap: adding a config module that includes file paths as constants

---
+++ main/config.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+VEP = "/dmp/hot/jayakumg/projects/germline-heme/vep/variant_effect_predictor.pl"
+VEP_CACHE = "/dmp/resources/prod/tools/bio/vep/VERSIONS/variant_effect_predictor_v86"
+PERL = "/dmp/resources/prod/tools/system/perl/bin/perl"
