695c3ecdff2fa9bc10a828d352275fd6e1f12a7c
andurill <gowtham.jn@gmail.com>
2020-05-19 01:05:10 -0400

    use production vep

--- main/config.py
+++ main/config.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-VEP = "/dmp/hot/jayakumg/projects/germline-heme/vep/variant_effect_predictor.pl"
+VEP = "/dmp/resources/prod/tools/bio/vep/VERSIONS/variant_effect_predictor_v86/variant_effect_predictor.pl"
 VEP_CACHE = "/dmp/resources/prod/tools/bio/vep/VERSIONS/variant_effect_predictor_v86"
 PERL = "/dmp/resources/prod/tools/system/perl/bin/perl"
 FASTA = "/dmp/resources/prod/tools/bio/vep/VERSIONS/variant_effect_predictor_v86/homo_sapiens_merged/86_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa"
